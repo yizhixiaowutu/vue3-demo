2b67f3d664b109e2d8272d7f716b595a8e08b4dc
tianxq3 <tianxq3@ziroom.com>
2023-11-16 09:03:25 +0800

    feat: 测试provide&inject

---
+++ src/views/children.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>子页面</h1>
+  <span>{{ numberRef }} - {{ numberConst }}</span>
+  <br />
+  <span>{{ reactiveObj.number1 }} - {{ reactiveObj.number2 }}</span>
+</template>
+
+<script setup lang="ts">
+import { inject, watch } from "vue";
+// const childNumber = ref(0);
+const { numberRef, numberConst } = inject("number");
+const reactiveObj = inject("reactiveObj");
+
+watch(numberRef, () => {
+  console.log("numberRef", numberRef.value);
+  console.log("numberConst", numberConst);
+});
+
+watch(
+  () => reactiveObj,
+  () => {
+    console.log("number2", reactiveObj.number2);
+  },
+  { deep: true }
+);
+watch(reactiveObj, () => {
+  console.log("reactiveObj", reactiveObj);
+});
+</script>
+
+<style scoped></style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -4,13 +4,29 @@
       <a-button @click="toG6">g6路由</a-button>
       <a-button @click="toFileSaver">file-saver路由</a-button>
       <a-button @click="toElementPlus">element-plus路由</a-button>
+      <a-button @click="number++">number + 1</a-button>
+      <a-button @click="number2++">number2 + 1</a-button>
     </div>
+    <Children />
   </div>
 </template>
 
 <script setup lang="ts">
+import { provide, reactive, ref } from "vue";
 import router from "@/router";
+import Children from "./children.vue";
 
+const number = ref(1);
+const number2 = ref(1);
+const reactiveObj = reactive({
+  number1: number,
+  number2: number2,
+});
+provide("reactiveObj", reactiveObj);
+provide("number", {
+  numberRef: number,
+  numberConst: 999,
+});
 const toG6 = () => {
   router.push("/g6");
 };
